124d56851edd7309396160abc87dbd57e99baff4
yuhaoK5 <38482051+yuhaoK5@users.noreply.github.com>
2018-08-21 14:01:40 +0800

    Update toEmail.py

--- toEmail.py
+++ toEmail.py
@@ -4,7 +4,6 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
 import xlrd
 
 EMAIL = '***********'  # 邮箱账号
